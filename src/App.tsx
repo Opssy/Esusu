e64140f319c078e1ed41b062c33ec7a5934ee51a
Opssy <opeyemib39@gmail.com>
2023-01-02 17:43:21 +0100

    fontend setup

--- src/App.tsx
+++ src/App.tsx
@@ -5,7 +5,7 @@ import './App.css';
 function App() {
   return (
     <div className="App">
-      <header className="App-header">
+      <header className="bg-black text-white text-3xl">
         <img src={logo} className="App-logo" alt="logo" />
         <p>
           Edit <code>src/App.tsx</code> and save to reload.
